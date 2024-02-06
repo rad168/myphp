cbdee31d3a6c70551f574ffeef1b13fd350507e8
rad168 <86744795+rad168@users.noreply.github.com>
2024-02-06 21:29:53 +0800

    Update tvb2024.php

--- api/tvb2024.php
+++ api/tvb2024.php
@@ -1,22 +1,20 @@
 <?php
 /*
 * GeJI恩山论坛
-    .php?id=0 翡翠台
-    .php?id=1 J2
-    .php?id=2 无线新闻台[1280x720]
-    .php?id=3 无线财*体育资讯台[1280x720]
-    .php?id=4 无线新闻台·海外版[1920x1080]
-    .php?id=5 無線新聞台·海外版360P
-    .php?id=6 无线财*体育资讯台·网络版[1920x1080]
-    .php?id=7 無線財經體育資訊台·海外版360P
-    .php?id=8 事件直播頻道1
-    .php?id=9 事件直播頻道1 360P
-    .php?id=10 事件直播頻道2
-    .php?id=11 事件直播頻道2 360P
+* GeJI恩山论坛
+*.php?id=0 无线新闻台[1280x720]
+*.php?id=1 无线财*体育资讯台[1280x720]
+*.php?id=2 无线新闻台·海外版[1920x1080]
+*.php?id=3 无线财*体育资讯台·网络版[1920x1080]
+*.php?id=4 事件直播1台[1280x720]
+*.php?id=5 事件直播2台[1280x720]
+*.php?id=6 无线新闻台[max1920x1080,min960x360,多画质多音轨DIYP不支持]
+*.php?id=7 无线财*体育资讯台[max1920x1080,min960x360,多画质多音轨DIYP不支持]
+*.php?id=8 事件直播1台[max1920x1080,min960x360,多画质多音轨DIYP不支持]
+*.php?id=9 事件直播2台[max1920x1080,min960x360,多画质多音轨DIYP不支持]
 */
 $id = $_GET['id'];
-//$ids = ['C','A','I-NEWS','I-FINA','NEVT1','NEVT2','C','A','NEVT1','NEVT2'];
-$ids = ['I-J','I-J2','C','A','I-NEWS','I-NEWS','I-FINA','I-FINA','NEVT1','NEVT1','NEVT2','NEVT2'];
+$ids = ['C','A','I-NEWS','I-FINA','NEVT1','NEVT2','C','A','NEVT1','NEVT2'];
 $header[] = 'CLIENT-IP:127.0.0.1';
 $header[] = 'X-FORWARDED-FOR:127.0.0.1';
 $ch = curl_init();
